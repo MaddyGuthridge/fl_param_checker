89b29621a3f589f0a5c126e530cd3a49fd2957b5
Miguel Guthridge <hdsq@outlook.com.au>
2022-09-29 01:12:21 +1000

    Add __init__.py to allow usage in other devices

---
+++ __init__.py
@@ -0,0 +1,18 @@
+"""
+# FL Plugin Param Checker Module
+
+Allows the fl_param_checker library to be used within other devices.
+
+Refer to README.md for usage.
+
+This code is free and open source under the MIT license. Refer to the LICENSE
+file for details.
+
+(c) 2022 Miguel Guthridge
+"""
+__all__ = [
+    'idleCallback',
+    'pluginParamCheck',
+]
+
+from .params import idleCallback, pluginParamCheck
